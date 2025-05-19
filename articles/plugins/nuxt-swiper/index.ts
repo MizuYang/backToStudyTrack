3dddd3c0e4ee4ad14e7aace7784d2b25903bd27b
工程部-楊濬智 <chuchyang@staff.1111.com.tw>
2025-05-19 09:15:46 +0800

    修正 文章物件: id, notion 參數

--- articles/plugins/nuxt-swiper/index.ts
+++ articles/plugins/nuxt-swiper/index.ts
@@ -1,7 +1,7 @@
 export const nuxtSwiperArticles = [
   {
     title: '照著 nuxt-swiper 官方文檔做研究',
-    notionCardId: '2025-0506-2210',
-    notionPath: 'Swiper_-1ebdcd513c4b80f380d0ecccabdd8906'
+    notionCardId: '2025-0519-2030',
+    notionPath: 'nuxt-swiper-1e6dcd513c4b8014bb55e576ab08cf23'
   }
 ]
