51a7ce065267cef550efdc0679f5a84d38c554c3
mizu <psdk55052004@gmail.com>
2025-05-24 02:22:18 +0800

    2025-0522-2330 gsap: gsap.to 建立第一個動畫!

--- components/plugins/gsap/2025-0522-2330/index.vue
+++ components/plugins/gsap/2025-0522-2330/index.vue
@@ -1,9 +1,20 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
+const { gsap } = useGsap()
 
+onMounted(async () => {
+  await nextTick()
+  gsap.to('.box-to', { x: 500 })
+})
 </script>
 
 <template>
-  <div>123</div>
+  <div class="m-5 flex flex-col items-start">
+    <div
+      class="box-to inline-block h-[100px] bg-purple-200 px-5 leading-[100px]"
+    >
+      <code>gsap.to('.box-to', { x: 500 })</code>
+    </div>
+  </div>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang="scss" scoped></style>
