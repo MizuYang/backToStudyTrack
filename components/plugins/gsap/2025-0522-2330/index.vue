780ff7315d49a8f18e712625bdda1772181ae7d1
mizu <psdk55052004@gmail.com>
2025-05-28 22:50:39 +0800

    2025-0522-2330 gsap/timeline: 常用的位置參數

--- components/plugins/gsap/2025-0522-2330/index.vue
+++ components/plugins/gsap/2025-0522-2330/index.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 const { gsap } = useGsap()
 
-onMounted(async () => {
-  await nextTick()
+const easePractice = (): void => {
   gsap.to('.box-to', { x: 500 })
   gsap.from('.box-from', { x: 500 })
   gsap.fromTo('.box-fromTo', { x: 500 }, { x: 250 })
@@ -21,51 +20,69 @@ onMounted(async () => {
     yoyo: true,
     repeat: -1
   })
-
-  const timeline1 = gsap.timeline()
-  timeline1.to('.box-timeline-1', {
+}
+const timeline1 = gsap.timeline()
+const timelineT1Fn = (): void => {
+  timeline1.to('.t1 .box-timeline-1', {
     x: () => window.innerWidth / 2,
     ease: 'expoScale(0.5,7,none)',
     yoyo: true,
     repeat: -1,
     duration: 2
   })
-  timeline1.to('.box-timeline-2', {
+  timeline1.to('.t1 .box-timeline-2', {
     x: () => window.innerWidth / 2,
     ease: 'expoScale(0.5,7,none)',
     yoyo: true,
     repeat: -1,
     duration: 1
   })
-  timeline1.to('.box-timeline-3', {
+  timeline1.to('.t1 .box-timeline-3', {
     x: () => window.innerWidth / 2,
     ease: 'expoScale(0.5,7,none)',
     yoyo: true,
     repeat: -1,
     duration: 1
   })
+}
+const timeline2 = gsap.timeline()
+const timelineT2Fn = (): void => {
+  timeline2.to('.t2 .box-timeline-1', { x: 500 })
+  timeline2.to('.t2 .box-timeline-2', { x: 500 }, 1)
+  timeline2.to('.t2 .box-timeline-3', { x: 500 }, '<')
+  timeline2.to('.t2 .box-timeline-4', { x: 500 }, '>')
+}
+
+onMounted(async () => {
+  await nextTick()
+
+  easePractice()
+  timelineT1Fn()
+  timelineT2Fn()
 })
 </script>
 
 <template>
-  <div class="m-5 flex flex-col items-start gap-y-3">
-    <div class="box box-to">
-      <code>gsap.to('.box-to', { x: 500 })</code>
-    </div>
-    <div class="box box-from">
-      <code>gsap.from('.box-from', { x: 500 })</code>
-    </div>
-    <div class="box box-fromTo">
-      <code>gsap.fromTo('.box-fromTo', { x: 500 }, { x: 250 })</code>
-    </div>
-    <div class="box box-set">
-      <code>gsap.set('.box-set', { x: 150 })</code>
-    </div>
-    <div class="box box-ease">
-      <code>ease: 'elastic.out(1,0.3)'</code>
-    </div>
+  <main class="m-5 flex flex-col items-start gap-y-3 py-10">
+    <section class="flex w-full flex-col items-start gap-y-3 bg-yellow-100">
+      <div class="box box-to">
+        <code>gsap.to('.box-to', { x: 500 })</code>
+      </div>
+      <div class="box box-from">
+        <code>gsap.from('.box-from', { x: 500 })</code>
+      </div>
+      <div class="box box-fromTo">
+        <code>gsap.fromTo('.box-fromTo', { x: 500 }, { x: 250 })</code>
+      </div>
+      <div class="box box-set">
+        <code>gsap.set('.box-set', { x: 150 })</code>
+      </div>
+      <div class="box box-ease">
+        <code>ease: 'elastic.out(1,0.3)'</code>
+      </div>
+    </section>
 
-    <div class="bg-blue-200/20 area-style">
+    <section class="area-style bg-blue-200/20">
       <div class="box box-staggers">
         stagger
       </div>
@@ -78,9 +95,9 @@ onMounted(async () => {
       <div class="box box-staggers">
         stagger
       </div>
-    </div>
+    </section>
 
-    <div class="bg-red-200/20 area-style">
+    <section class="t1 area-style bg-red-200/20">
       <div class="box box-timeline-1">
         gsap.timeline()
       </div>
@@ -90,8 +107,37 @@ onMounted(async () => {
       <div class="box box-timeline-3">
         gsap.timeline()
       </div>
-    </div>
-  </div>
+    </section>
+
+    <section class="t2 area-style bg-amber-200/20">
+      <button
+        type="button"
+        class="bg-blue-400 px-2 py-1 text-white"
+        @click="timeline2.restart()"
+      >
+        重新執行時間軸
+      </button>
+      <div class="box box-timeline-1">
+        1 直接執行,
+        <code class="ml-5 bg-black/80 px-2 py-1 text-white"> { x: 500 }</code>
+      </div>
+      <div class="box box-timeline-2">
+        2 一秒後執行,
+        <code class="ml-5 bg-black/80 px-2 py-1 text-white">
+          { x: 500 }, 1</code>
+      </div>
+      <div class="box box-timeline-3">
+        3 與上一個一起執行,
+        <code class="ml-5 bg-black/80 px-2 py-1 text-white">
+          { x: 500 }, '&lt;'</code>
+      </div>
+      <div class="box box-timeline-4">
+        4 上一個結束後執行,
+        <code class="ml-5 bg-black/80 px-2 py-1 text-white">
+          { x: 500 }, '>'</code>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style lang="scss" scoped>
@@ -99,6 +145,6 @@ onMounted(async () => {
   @apply inline-block h-[100px] bg-gray-300 px-5 leading-[100px];
 }
 .area-style {
-  @apply w-full flex flex-col items-start gap-2;
+  @apply flex w-full flex-col items-start gap-2;
 }
 </style>
