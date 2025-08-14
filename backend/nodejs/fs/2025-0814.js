0fc657a8b916f7beffa34936eead23401ace90c0
MizuYang <psdk55052004@gmail.com>
2025-08-14 12:44:33 +0800

    2025-0814 nodejs/fs: appendFileSync 追加寫入(同步)

--- backend/nodejs/fs/2025-0814.js
+++ backend/nodejs/fs/2025-0814.js
@@ -1,4 +1,4 @@
-import { writeFile, writeFileSync } from "fs";
+import { appendFile, appendFileSync, writeFile, writeFileSync } from "fs";
 
 console.clear()
 
@@ -23,14 +23,22 @@ console.clear()
 
 
 // 追加寫入 (非同步)
-import { appendFile } from 'fs'
 
-appendFile('./files/追加.txt', `\r\n寫入內容 ${new Date().toLocaleString()}`, err => {
-  if (err) {
-    console.error('1 追加寫入失敗:', err);
-    return;
-  }
-  console.log('1 追加寫入成功!');
-})
+// appendFile('./files/追加.txt', `\r\n寫入內容 ${new Date().toLocaleString()}`, err => {
+//   if (err) {
+//     console.error('1 追加寫入失敗:', err);
+//     return;
+//   }
+//   console.log('1 追加寫入成功!');
+// })
 
-console.log("3 執行 (這裡會比 追加寫入(非同步) 先執行)");
+
+// 追加寫入 (同步)
+try {
+  appendFileSync('./files/追加.txt', '\r\n寫入內容 ' + new Date().toLocaleString());
+  console.log('2 追加寫入成功!');
+} catch (err) {
+  console.error('2 追加寫入失敗:', err);
+}
+
+console.log("3 執行");
