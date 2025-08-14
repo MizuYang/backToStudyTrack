6dc78b6e76a15d810d37c61ee2365ef2c7d2ec19
MizuYang <psdk55052004@gmail.com>
2025-08-14 12:39:51 +0800

    2025-0814 nodejs/fs: appendFile 追加寫入(非同步)

--- backend/nodejs/fs/2025-0814.js
+++ backend/nodejs/fs/2025-0814.js
@@ -1,20 +1,36 @@
 import { writeFile, writeFileSync } from "fs";
 
+console.clear()
+
 // 非同步寫入
-writeFile("./files/2025-0814-writeFile.txt", "Hello World!!", (err) => {
-  if (err) {
-    console.error("1 寫入檔案失敗:", err);
-    return;
-  }
-  console.log("1 檔案寫入成功!");
-});
+// writeFile("./files/2025-0814-writeFile.txt", "Hello World!!", (err) => {
+//   if (err) {
+//     console.error("1 寫入檔案失敗:", err);
+//     return;
+//   }
+//   console.log("1 檔案寫入成功!");
+// });
 
 // 同步寫入
-try {
-  writeFileSync("./files/2025-0814-writeFileSync.txt", "Hello World!!");
-  console.log("2 檔案寫入成功!");
-} catch (err) {
-  console.error("2 寫入檔案失敗:", err);
-}
+// try {
+//   writeFileSync("./files/2025-0814-writeFileSync.txt", "Hello World!!");
+//   console.log("2 檔案寫入成功!");
+// } catch (err) {
+//   console.error("2 寫入檔案失敗:", err);
+// }
 
-console.log("執行 (這裡會比 非同步寫入 先執行)");
+// console.log("執行 (這裡會比 非同步寫入 先執行)");
+
+
+// 追加寫入 (非同步)
+import { appendFile } from 'fs'
+
+appendFile('./files/追加.txt', `\r\n寫入內容 ${new Date().toLocaleString()}`, err => {
+  if (err) {
+    console.error('1 追加寫入失敗:', err);
+    return;
+  }
+  console.log('1 追加寫入成功!');
+})
+
+console.log("3 執行 (這裡會比 追加寫入(非同步) 先執行)");
