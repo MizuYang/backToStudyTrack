3e742c51f4f92b9a04e91eaf826e8a5fd6bde542
MizuYang <psdk55052004@gmail.com>
2025-08-14 12:34:23 +0800

    2025-0814 nodejs/fs: writeFileSync

--- backend/nodejs/fs/2025-0814.js
+++ backend/nodejs/fs/2025-0814.js
@@ -1,10 +1,20 @@
-import { writeFile } from 'fs'
+import { writeFile, writeFileSync } from "fs";
 
 // 非同步寫入
-writeFile('./2025-0814-helloWorld.txt', 'Hello World!!', (err) => {
+writeFile("./files/2025-0814-writeFile.txt", "Hello World!!", (err) => {
   if (err) {
-    console.error(err)
-    return
+    console.error("1 寫入檔案失敗:", err);
+    return;
   }
-  console.log('1 檔案寫入成功!')
-})
+  console.log("1 檔案寫入成功!");
+});
+
+// 同步寫入
+try {
+  writeFileSync("./files/2025-0814-writeFileSync.txt", "Hello World!!");
+  console.log("2 檔案寫入成功!");
+} catch (err) {
+  console.error("2 寫入檔案失敗:", err);
+}
+
+console.log("執行 (這裡會比 非同步寫入 先執行)");
