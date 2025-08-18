c8571e8e8137473451d3700bf1d0f75a30a4c1db
MizuYang <psdk55052004@gmail.com>
2025-08-18 09:09:07 +0800

    2025-0815 nodejs/fs: 創建、讀取、刪除 資料夾

--- backend/nodejs/fs/2025-0818.js
+++ backend/nodejs/fs/2025-0818.js
@@ -1,18 +1,57 @@
-import { unlink, rm } from 'fs'
-
 // 刪除文件
-unlink('./files/刪我1.txt', (err) => {
-  if (err) {
-    console.error('刪除檔案失敗:', err)
-    return
-  }
-  console.log('檔案刪除成功!')
-})
+// import { unlink, rm } from 'fs'
 
-rm('./files/刪我2.txt', (err) => {
+// unlink('./files/刪我1.txt', (err) => {
+//   if (err) {
+//     console.error('刪除檔案失敗:', err)
+//     return
+//   }
+//   console.log('檔案刪除成功!')
+// })
+
+// rm('./files/刪我2.txt', (err) => {
+//   if (err) {
+//     console.error('刪除檔案失敗:', err)
+//     return
+//   }
+//   console.log('檔案刪除成功!')
+// })
+
+// 創建資料夾
+// import { mkdir } from 'fs'
+// mkdir('./newFolder', (err) => {
+//   if (err) {
+//     console.error('創建資料夾失敗:', err)
+//     return
+//   }
+//   console.log('資料夾創建成功!')
+// })
+// 遞歸創建資料夾
+// mkdir('./newFolder/a/b', { recursive: true }, (err) => {
+//   if (err) {
+//     console.error('創建資料夾失敗:', err)
+//     return
+//   }
+//   console.log('資料夾創建成功!')
+// })
+
+// 讀取資料夾
+// import { readdir } from 'fs'
+// readdir('./files', (err, data) => {
+//   if (err) {
+//     console.error('讀取資料夾失敗:', err)
+//     return
+//   }
+//   console.log('資料夾內容:', data)
+// })
+
+// 刪除資料夾
+import { rm } from 'fs'
+// 遞歸刪除資料夾
+rm('./newFolder', { recursive: true }, (err) => {
   if (err) {
-    console.error('刪除檔案失敗:', err)
+    console.error('刪除資料夾失敗:', err)
     return
   }
-  console.log('檔案刪除成功!')
+  console.log('資料夾刪除成功!')
 })
