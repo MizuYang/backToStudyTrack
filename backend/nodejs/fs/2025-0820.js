89a8705533fa1e9558f2bee58115f98f869b768c
MizuYang <psdk55052004@gmail.com>
2025-08-20 08:32:46 +0800

    2025-0815 nodejs/path: 網址解析練習

--- backend/nodejs/fs/2025-0820.js
+++ backend/nodejs/fs/2025-0820.js
@@ -7,3 +7,10 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 // 絕對路徑: /Users/chuchyang/Desktop/...略/fs/index.html
 console.log(path.resolve(__dirname, "./index.html"));
 
+// 網址解析
+// https://search.js.com:443/search?keyword=xxx&psort=3
+// 協議名: https
+// 主機名: search.js.com
+// 端口號: 443
+// 路徑: /search
+// 查詢字串: ?keyword=xxx&psort=3
