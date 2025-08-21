2a160a29fc935380a82bca5337833a5fe1ed9ffc
MizuYang <psdk55052004@gmail.com>
2025-08-21 13:09:13 +0800

    2025-0821 nodejs/http: 取得請求中的 url 路徑、query 字串 (方法二)

--- backend/nodejs/fs/2025-0821.js
+++ backend/nodejs/fs/2025-0821.js
@@ -39,20 +39,40 @@ console.clear();
 // });
 
 // 取得請求中的 url 路徑、query 字串 - 方法一
+// import http from "http";
+// import url from "url";
+
+// const server = http.createServer((req, res) => {
+//   const parseUrl = url.parse(req.url, true);
+//   // console.log("parseUrl: ", parseUrl);
+
+//   // 取得 query
+//   const { query } = parseUrl;
+//   console.log("query: ", query);
+
+//   // 取得路徑
+//   const { pathname } = parseUrl;
+//   console.log("pathname: ", pathname);
+
+//   res.end("Hello World");
+// });
+
+// server.listen(3000, () => {
+//   console.log("伺服器開始連接... http://localhost:3000");
+// });
+
+// 取得請求中的 url 路徑、query 字串 - 方法二(推薦)
 import http from "http";
-import url from "url";
 
 const server = http.createServer((req, res) => {
-  const parseUrl = url.parse(req.url, true);
-  // console.log("parseUrl: ", parseUrl);
+  console.log(req.headers);
+  const url = new URL(req.url, `http://${req.headers.host}`);
 
   // 取得 query
-  const { query } = parseUrl;
-  console.log("query: ", query);
-
+  const search = url.searchParams.get("search"); // 這裡依據實際的 query 參數名稱來獲取
+  console.log("search: ", search);
   // 取得路徑
-  const { pathname } = parseUrl;
-  console.log("pathname: ", pathname);
+  console.log(url.pathname);
 
   res.end("Hello World");
 });
