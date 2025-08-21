65c47ecdbba865a9736fc946f006659e15955c5c
MizuYang <psdk55052004@gmail.com>
2025-08-21 12:52:13 +0800

    2025-0821 nodejs/http: 取得請求中的 url 路徑、query 字串

--- backend/nodejs/fs/2025-0821.js
+++ backend/nodejs/fs/2025-0821.js
@@ -1,35 +1,58 @@
-import http from "http";
+// import http from "http";
 
 console.clear();
 
+// const server = http.createServer((req, res) => {
+//   // 取得網址
+//   // console.log("網址", req.url);
+
+//   // 取得請求方法
+//   // console.log("請求方法", req.method);
+
+//   // 取得請求版本
+//   // console.log("請求版本", req.httpVersion);
+
+//   // 取得請求標頭
+//   // console.log("請求標頭", req.headers);
+
+//   // 取得請求主機
+//   // console.log("請求主機", req.headers.host);
+
+//   // 取得請求體
+//   // console.log("請求體", req.body);
+
+//   // 讀取請求體資料事件
+//   req.on("data", (chunk) => {
+//     console.log("請求體資料塊:", chunk.toString());
+//   });
+
+//   // 請求結束後觸發的事件
+//   req.on("end", () => {
+//     console.log("請求結束");
+//   });
+
+//   res.end("Hello World");
+// });
+
+// server.listen(3000, () => {
+//   console.log("伺服器開始連接... http://localhost:3000");
+// });
+
+// 取得請求中的 url 路徑、query 字串 - 方法一
+import http from "http";
+import url from "url";
+
 const server = http.createServer((req, res) => {
-  // 取得網址
-  // console.log("網址", req.url);
+  const parseUrl = url.parse(req.url, true);
+  // console.log("parseUrl: ", parseUrl);
 
-  // 取得請求方法
-  // console.log("請求方法", req.method);
+  // 取得 query
+  const { query } = parseUrl;
+  console.log("query: ", query);
 
-  // 取得請求版本
-  // console.log("請求版本", req.httpVersion);
-
-  // 取得請求標頭
-  // console.log("請求標頭", req.headers);
-
-  // 取得請求主機
-  // console.log("請求主機", req.headers.host);
-
-  // 取得請求體
-  // console.log("請求體", req.body);
-
-  // 讀取請求體資料事件
-  req.on("data", (chunk) => {
-    console.log("請求體資料塊:", chunk.toString());
-  });
-
-  // 請求結束後觸發的事件
-  req.on("end", () => {
-    console.log("請求結束");
-  });
+  // 取得路徑
+  const { pathname } = parseUrl;
+  console.log("pathname: ", pathname);
 
   res.end("Hello World");
 });
