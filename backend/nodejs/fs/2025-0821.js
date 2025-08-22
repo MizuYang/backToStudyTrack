eb44b09399dfa2f9cddf40f14e1b45812fce2d0b
MizuYang <psdk55052004@gmail.com>
2025-08-22 08:42:49 +0800

    2025-0822 nodejs/http: 請求練習, 依頁面路徑回傳對應 html 頁面

--- backend/nodejs/fs/2025-0821.js
+++ backend/nodejs/fs/2025-0821.js
@@ -62,19 +62,54 @@ console.clear();
 // });
 
 // 取得請求中的 url 路徑、query 字串 - 方法二(推薦)
+// import http from "http";
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.headers);
+//   const url = new URL(req.url, `http://${req.headers.host}`);
+
+//   // 取得 query
+//   const search = url.searchParams.get("search"); // 這裡依據實際的 query 參數名稱來獲取
+//   console.log("search: ", search);
+//   // 取得路徑
+//   console.log(url.pathname);
+
+//   res.end("Hello World");
+// });
+
+// server.listen(3000, () => {
+//   console.log("伺服器開始連接... http://localhost:3000");
+// });
+
+// 請求練習
+/**
+ * 功能敘述：
+ * 請求路徑為 /login, 回傳 登入頁面
+ * 請求路徑為 /register, 回傳 註冊頁面
+ * 其他路徑回傳 404 Not Found
+ */
+import fs from "fs";
 import http from "http";
+import url from "url";
+import path from "path";
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  const url = new URL(req.url, `http://${req.headers.host}`);
-
-  // 取得 query
-  const search = url.searchParams.get("search"); // 這裡依據實際的 query 參數名稱來獲取
-  console.log("search: ", search);
   // 取得路徑
-  console.log(url.pathname);
+  const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+  const { pathname } = reqUrl;
+  const filePath = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filePath);
+  const fileName = pathname.slice(1);
 
-  res.end("Hello World");
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  try {
+    // 讀取要回傳的檔案
+    const page = fs.readFileSync(`${dirname}/files/${fileName}.html`);
+    res.end(page);
+  } catch (err) {
+    res.end("找不到頁面");
+  }
 });
 
 server.listen(3000, () => {
