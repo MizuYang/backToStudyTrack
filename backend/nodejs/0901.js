f51d84cd421de716057b13f014dfef1245982c20
MizuYang <psdk55052004@gmail.com>
2025-09-01 10:18:14 +0800

    2025-0901 nodejs: express app.all

--- backend/nodejs/0901.js
+++ backend/nodejs/0901.js
@@ -4,11 +4,23 @@ const app = express();
 
 // 請求方法是 get, 路徑是 /, 執行對應的處理函式
 app.get("/", (req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-  console.log(req.ip);
-
   res.send("Hello World");
+});
+
+// .all 可以接受所有的請求方法
+app.all("/home", (req, res) => {
+  res.send("home");
+});
+
+// 使用 * 處理所有未定義的路由
+// app.all("*", (req, res) => {
+//   res.send("Not Found");
+// });
+
+// 使用 app.use 處理所有未定義的路由
+app.use((req, res) => {
+  res.status(404);
+  res.send("Not Found");
 });
 
 app.listen("8080", () => {
