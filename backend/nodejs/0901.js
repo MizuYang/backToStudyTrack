20266b615bc7c3566c3ed7f062d75957afda9258
MizuYang <psdk55052004@gmail.com>
2025-09-02 09:02:54 +0800

    2025-0902 nodejs: express request.params

--- backend/nodejs/0901.js
+++ backend/nodejs/0901.js
@@ -7,6 +7,36 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/request/:id", (req, res) => {
+  // http://localhost:8080/request?name=mizu&age=18&pets=8cat,dog
+  console.log("req.method", req.method);
+  console.log("req.url", req.url);
+  console.log("req.httpVersion", req.httpVersion);
+  console.log("req.headers", req.headers);
+
+  // express 相關方法
+  console.log("req.path", req.path);
+  console.log("req.query", req.query);
+  console.log("req.hostname", req.hostname);
+  console.log('req.params', req.params.id);
+  // 獲取 ip
+  console.log("req.ip", req.ip);
+  console.log("req.ips", req.ips);
+  // 獲取請求頭
+  console.log('User-Agent', req.get('User-Agent'));
+  console.log('Content-Type', req.get('Content-Type'));
+  console.log('host', req.get('host'));
+  // 獲取子網域 (假設有設定子網域)
+  console.log("req.subdomains", req.subdomains);
+
+  // 設定狀態碼
+  res.status(201);
+  // 設定回應標頭 (X-Powered-By 通常用來表明支援網站的技術，不過在生產環境中，出於安全考量，有時會移除或修改此標頭，以避免洩露伺服器技術資訊。)
+  res.set("X-Powered-By", "Node.js");
+  // 回應內容
+  res.send("Hello World");
+});
+
 // .all 可以接受所有的請求方法
 app.all("/home", (req, res) => {
   res.send("home");
