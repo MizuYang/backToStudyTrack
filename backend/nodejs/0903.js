a848d3757a47b16efdc715aaa09ffd3c7aed6c10
MizuYang <psdk55052004@gmail.com>
2025-09-03 15:05:22 +0800

    2025-0903 nodejs: express 回應方法 redirect, download, json, sendFile

--- backend/nodejs/0903.js
+++ backend/nodejs/0903.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import url from "url";
 
 const app = express();
 
@@ -13,6 +15,27 @@ app.get("/response", (req, res) => {
   res.status(201);
   res.set("aaa", "bbb");
   res.send("泥好！ Express"); // 這邊不會出現亂碼，因為 Express 預設會幫你設定編碼
+});
+
+app.get("/", (req, res) => {
+  // 重定向
+  // res.redirect("/redirect-page");
+
+  // 下載檔案
+  // res.download('./ai.png')
+
+  // json
+  // res.json({ name: "Mizu", age: 18 });
+
+  // sendFile 回應檔案內容
+  const filePath = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filePath);
+  const formPath = path.resolve(dirname, "./form.html");
+  res.sendFile(formPath);
+});
+
+app.get("/redirect-page", (req, res) => {
+  res.send("你被導向到這個頁面了");
 });
 
 app.listen(8888, () => {
