bcd70dbd2095cd82c6ae50dd81ca80b43261a070
MizuYang <psdk55052004@gmail.com>
2025-11-26 10:48:30 +0800

    2025-1126 nodejs: todoList/validator 全部匯入、單一匯入、驗證 email、字串長度

--- backend/nodejs/jwt/1126/index.js
+++ backend/nodejs/jwt/1126/index.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcryptjs";
+import validator from "validator";
+import isLength from "validator/lib/isLength.js"; // 單獨引入 isLength 函式
 
 // bcrypt 示範
 const password = "mizuNo123!";
@@ -19,3 +21,9 @@ const hash = await bcrypt.hash(password, salt);
  * salt $2b$12$SuXyRVuxUwlcXzEb6GlFiO
    hash $2b$12$SuXyRVuxUwlcXzEb6GlFiO3ooldPGF7qLlHFdWcoLUSAVgB7C91d.
  */
+
+// validator 示範
+// 是否為有效的電子郵件格式
+console.log(await validator.isEmail("8catCute@bar.com")); // true
+// 是否為有效的字串長度
+console.log(isLength("8catCute", { min: 5, max: 20 })); // true
