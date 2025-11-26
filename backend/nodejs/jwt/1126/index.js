3cefa75616ee973a278b95849d2a1fd4b624ffc7
MizuYang <psdk55052004@gmail.com>
2025-11-26 08:40:15 +0800

    2025-1126 nodejs: todoList/bcryptjs 加鹽

--- backend/nodejs/jwt/1126/index.js
+++ backend/nodejs/jwt/1126/index.js
@@ -5,8 +5,17 @@ const password = "mizuNo123!";
 
 // 密碼加密
 const hashedPassword = await bcrypt.hash(password, 12);
-console.log("hashedPassword: ", hashedPassword);
+// console.log("hashedPassword: ", hashedPassword);
 
 // 密碼比較是否一致
 const isMatch = await bcrypt.compare(password, hashedPassword);
-console.log("密碼是否一致: ", isMatch);
+// console.log("密碼是否一致: ", isMatch);
+
+const salt = await bcrypt.genSalt(12);
+const hash = await bcrypt.hash(password, salt);
+// console.log("salt", salt);
+// console.log("hash", hash);
+/**
+ * salt $2b$12$SuXyRVuxUwlcXzEb6GlFiO
+   hash $2b$12$SuXyRVuxUwlcXzEb6GlFiO3ooldPGF7qLlHFdWcoLUSAVgB7C91d.
+ */
