a59480b8a63eae39a2a5155696c3a968127af22d
MizuYang <psdk55052004@gmail.com>
2025-11-28 08:40:05 +0800

    2025-1128 nodejs: JWT/jsonwebtoken 加解密

--- backend/nodejs/jwt/1126/index.js
+++ backend/nodejs/jwt/1126/index.js
@@ -1,6 +1,16 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import validator from "validator";
 import isLength from "validator/lib/isLength.js"; // 單獨引入 isLength 函式
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 載入專案根目錄的 .env.dev 環境變數
+dotenv.config({ path: path.resolve(__dirname, "../../../../.env.dev") });
 
 // bcrypt 示範
 const password = "mizuNo123!";
@@ -24,9 +34,9 @@ const hash = await bcrypt.hash(password, salt);
 
 // validator 示範
 // 是否為有效的電子郵件格式
-console.log(await validator.isEmail("8catCute@bar.com")); // true
+// console.log(await validator.isEmail("8catCute@bar.com")); // true
 // 是否為有效的字串長度
-console.log(isLength("8catCute", { min: 5, max: 20 })); // true
+// console.log(isLength("8catCute", { min: 5, max: 20 })); // true
 
 const strongPassword = "MyPass123";
 
@@ -38,4 +48,50 @@ const isValid = await validator.isStrongPassword(strongPassword, {
   minSymbols: 0, // 不要求特殊符號(預設是 1)
 });
 
-console.log("強密碼驗證結果: ", isValid); // true 或 false
+// console.log("強密碼驗證結果: ", isValid); // true 或 false
+
+const PAYLOAD = {
+  userName: "Mizu",
+  role: "admin",
+  pets: ["cat", "dog"],
+};
+
+const DEFAULT_TEST_JWT_SECRET = "qw~f21!@@!123"; // 隨便打的測試用 secret XD
+const SECRET = process.env.NUXT_PUBLIC_JWT_SECRET || DEFAULT_TEST_JWT_SECRET;
+
+// 1. 建立 JWT TOKEN
+const TOKEN = await new Promise((resolve, reject) => {
+  jwt.sign(
+    PAYLOAD,
+    SECRET,
+    {
+      expiresIn: process.env.NUXT_PUBLIC_JWT_EXPIRES_DAYS || "7d",
+    },
+    (err, token) => {
+      if (err) {
+        console.error("JWT 建立失敗: ", err);
+        reject(err);
+      } else {
+        resolve(token);
+      }
+    },
+  );
+});
+console.log(
+  "可將 JWT TOKEN 和 JWT SECRET 丟到 https://www.jwt.io/ 觀看解碼結果: ",
+);
+console.log("JWT TOKEN: ", TOKEN);
+console.log("JWT SECRET: ", SECRET);
+
+// 2. 驗證 JWT TOKEN 並解碼
+const decode = await new Promise((resolve, reject) => {
+  jwt.verify(TOKEN, SECRET, (err, decoded) => {
+    if (err) {
+      console.error("JWT 驗證失敗: ", err);
+      reject(err);
+    } else {
+      resolve(decoded);
+    }
+  });
+});
+console.log("解碼後的 JWT 資料: ", decode);
